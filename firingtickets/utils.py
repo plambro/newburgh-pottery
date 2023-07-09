df88263df2afbc2afdee48e2783f36f9d0208111
Peter Lambro <peterlambro@gmail.com>
2023-07-09 10:27:35 -0400

    Fixed name ref

--- firingtickets/utils.py
+++ firingtickets/utils.py
@@ -6,11 +6,10 @@ def print_ticket(ticket):
     try:
         printer_name = settings.PRINTERNAME
         conn = cups.Connection()
-
-        job = f'Ticket for {ticket.name} at {django_now()}'
+        name_string = f'{ticket.first_name} {ticket.last_name}'
+        job = f'Ticket for {name_string} at {django_now()}'
         filename = 'print_job.txt'
         title_string = 'Newburgh Pottery Ticket'
-        name_string = f'{ticket.first_name} {ticket.last_name}'
         details_string = f'L{ticket.length} H{ticket.height} W{ticket.width}'
         id_string = f'{ticket.id}'
         printable_string = f'{title_string} \n\n{name_string} \n\n{ticket.membership} \n\n{details_string} \n\nTotalCost: {ticket.total_cost()} \n\nQuantity: {ticket.quantity} \n\nCreated: {ticket.created} \n\nTicket ID: {id_string}\n\n '
